d01ccab85fa547766c7c40745ccd8213066a289c
Alex Sampley <alex.sampley@gmail.com>
2022-05-10 19:30:25 +0000

    Add yt-dlp to install script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,1 +1,4 @@
-sudo apt install build-essential pkg-config libssl-dev ffmpeg youtube-dl autoconf libtool
+sudo apt install build-essential pkg-config libssl-dev ffmpeg autoconf libtool
+
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
