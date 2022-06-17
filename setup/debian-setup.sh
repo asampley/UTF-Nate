d8b67184c12ae7c95548b16e5b6723cadec57ccd
Alex Sampley <alex.sampley@gmail.com>
2022-06-16 19:58:24 -0700

    Added a script to help setup a postgres database

---
+++ setup/debian-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -euf
+
+# NOTE
+# This is not meant to be an automatic process, and will prompt for acceptance of the apt packages.
+# Confirm yourself that it's okay, or pipe in yes to skip
+
+# Install packages required for building, playing audio, and youtube-dl
+# Remove python if you don't need youtube-dl support
+# Remove postgresql if you don't require the database to be installed
+sudo apt install build-essential pkg-config libssl-dev ffmpeg autoconf libtool python postgresql
+
+# Add one of these youtube dl libraries to your install.
+# At time of writing, youtube-dlc proved to be the most functional and stable.
+# These require python.
+
+## yt-dlp install
+#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+#sudo chmod a+rx /usr/local/bin/yt-dlp
+
+# youtube-dlc install
+sudo curl -L https://github.com/blackjack4494/yt-dlc/releases/latest/download/youtube-dlc -o /usr/local/bin/youtube-dlc
+sudo chmod a+rx /usr/local/bin/youtube-dlc
--- ubuntu-setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install build-essential pkg-config libssl-dev ffmpeg autoconf libtool
-
-# yt-dlp install
-#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-#sudo chmod a+rx /usr/local/bin/yt-dlp
-
-# youtube-dlc install
-sudo curl -L https://github.com/blackjack4494/yt-dlc/releases/latest/download/youtube-dlc -o /usr/local/bin/youtube-dlc
-sudo chmod a+rx /usr/local/bin/youtube-dlc
