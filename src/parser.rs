51faa72c892caa7ebff0d24750ea60fcfd4a18f1
Alex Sampley <alex.sampley@gmail.com>
2023-08-18 20:16:37 -0600

    Add parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -11,10 +11,10 @@ use std::ops::RangeInclusive;
 
 use nom::{
 	branch::alt,
-	character::complete::{char as cchar, u32 as cu32},
+	character::complete::{char as cchar, multispace0, u64 as cu64},
 	combinator::{all_consuming, map},
 	multi::separated_list0,
-	sequence::separated_pair,
+	sequence::{delimited, separated_pair},
 	Finish, IResult, ToUsize,
 };
 
@@ -67,21 +67,32 @@ impl std::str::FromStr for Selection<usize> {
 	}
 }
 
-/// [`nom`] style parser for turning a u32 into a usize.
+/// [`nom`] style parser for turning a u64 into a usize.
 pub fn cusize(input: &str) -> IResult<&str, usize> {
-	map(cu32, |v| v.to_usize())(input)
+	map(cu64, |v| v.to_usize())(input)
 }
 
 /// [`nom`] style parser for turning "`usize`-`usize`" into a
 /// [`RangeInclusive<usize>`].
 pub fn range_usize(input: &str) -> IResult<&str, RangeInclusive<usize>> {
-	map(separated_pair(cusize, cchar('-'), cusize), |(a, b)| a..=b)(input)
+	map(
+		separated_pair(
+			cusize,
+			delimited(multispace0, cchar('-'), multispace0),
+			cusize,
+		),
+		|(a, b)| a..=b,
+	)(input)
 }
 
 /// [`nom`] style parser for parsing either a range or num. See [`cusize`] and
 /// [`range_usize`].
 pub fn num_or_range_usize(input: &str) -> IResult<&str, NumOrRange<usize>> {
-	alt((map(range_usize, |v| v.into()), map(cusize, |v| v.into())))(input)
+	delimited(
+		multispace0,
+		alt((map(range_usize, |v| v.into()), map(cusize, |v| v.into()))),
+		multispace0,
+	)(input)
 }
 
 /// [`nom`] style parser for getting a list of nums and ranges separated by
@@ -90,4 +101,81 @@ pub fn selection(input: &str) -> IResult<&str, Selection<usize>> {
 	all_consuming(map(separated_list0(cchar(','), num_or_range_usize), |v| {
 		v.into()
 	}))(input)
+}
+
+#[cfg(test)]
+mod test {
+	use itertools::Itertools;
+
+	use super::*;
+
+	#[test]
+	fn selection_num() {
+		for num in [usize::MIN, 1, 20, usize::MAX] {
+			let s = num.to_string();
+
+			let sel = selection(&s).expect("Error parsing");
+
+			assert_eq!(vec![NumOrRange::Num(num)], sel.1 .0)
+		}
+	}
+
+	#[test]
+	fn selection_range() {
+		for range in [0..=0, 0..=10, 3..=5, usize::MIN..=usize::MAX] {
+			for left_space in ["", " "] {
+				for right_space in ["", " "] {
+					let s = format!(
+						"{}{}-{}{}",
+						range.start(),
+						left_space,
+						right_space,
+						range.end()
+					);
+
+					let sel = selection(&s).expect("Error parsing");
+
+					assert_eq!(vec![NumOrRange::Range(range.clone())], sel.1 .0)
+				}
+			}
+		}
+	}
+
+	#[test]
+	fn selection_list() {
+		let target = [
+			NumOrRange::Num(usize::MIN),
+			NumOrRange::Range(6..=7),
+			NumOrRange::Num(usize::MAX),
+			NumOrRange::Range(usize::MAX..=usize::MAX),
+			NumOrRange::Num(5),
+		];
+
+		["", " "]
+			.into_iter()
+			.cartesian_product(["", " "])
+			.map(|(a, b)| {
+				target
+					.iter()
+					.map(|t| {
+						(
+							a,
+							match t {
+								NumOrRange::Range(range) => {
+									format!("{}-{}", range.start(), range.end())
+								}
+								NumOrRange::Num(num) => num.to_string(),
+							},
+							b,
+						)
+					})
+					.map(|(a, t, b)| format!("{}{}{}", a, t, b))
+					.join(",")
+			})
+			.for_each(|s| {
+				let sel = selection(&s).expect("Error parsing");
+
+				assert_eq!(target, sel.1 .0[..])
+			});
+	}
 }
