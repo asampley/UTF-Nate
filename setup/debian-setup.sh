c6daa3caf79867ccc830d13e253b654218fee71d
Alex Sampley <alex.sampley@gmail.com>
2023-12-01 19:33:47 -0700

    Remove ffmpeg in setup instructions

--- setup/debian-setup.sh
+++ setup/debian-setup.sh
@@ -8,18 +8,18 @@ set -euf
 # Install packages required for building, playing audio, and youtube-dl
 # Remove python if you don't need youtube-dl support
 # Remove postgresql if you don't require the database to be installed
-sudo apt install build-essential pkg-config libssl-dev ffmpeg autoconf libtool python postgresql cmake
+sudo apt install build-essential pkg-config libssl-dev autoconf libtool python postgresql cmake
 
 # Add one of these youtube-dl programs to your install.
 # These require python.
 
 ## youtube-dl install
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
+#sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+#sudo chmod a+rx /usr/local/bin/youtube-dl
 
-## yt-dlp install
-#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-#sudo chmod a+rx /usr/local/bin/yt-dlp
+# yt-dlp install
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
 
 ## youtube-dlc install
 #sudo curl -L https://github.com/blackjack4494/yt-dlc/releases/latest/download/youtube-dlc -o /usr/local/bin/youtube-dlc
